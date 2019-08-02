752be1c5ae9afc114f89bb850dca0d01cbf6c029
yachimoto <yachimoto0607@gmail.com>
2019-08-02 11:15:04 +0800

    Firstcomiit

---
+++ whatshouldIdo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whatshouldIdo
+//
+//  Created by SeitoYachimoto on 2019/08/02.
+//  Copyright © 2019 谷地元　星斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
