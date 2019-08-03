242329f4acefe92a773b8fe6ffa969927d266f11
yachimoto <yachimoto0607@gmail.com>
2019-08-03 17:16:58 +0800

    画面遷移してないところで作業終了

--- whatshouldIdo/AppDelegate.swift
+++ whatshouldIdo/AppDelegate.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //        IQKeyboardManagerを有効化する。
+        IQKeyboardManager.shared.enable = true
         return true
     }
 
